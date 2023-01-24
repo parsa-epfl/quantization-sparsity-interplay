d8d117ffac76e867cfa3882e542979bf0e81929c
Ayan Chakraborty <ayan.chakraborty@epfl.ch>
2023-01-24 02:34:18 +0100

    DsfsdF

--- examples/pytorch/image-classification/run.sh
+++ examples/pytorch/image-classification/run.sh
@@ -1,17 +1,24 @@
-python3 run_image_classification.py \
-    --dataset_name cifar100 \
-    --output_dir ./cifar100_test/ \
-    --remove_unused_columns False \
-    --do_train \
-    --do_eval \
-    --learning_rate 2e-5 \
-    --num_train_epochs 5 \
-    --per_device_train_batch_size 8 \
-    --per_device_eval_batch_size 8 \
-    --logging_strategy steps \
-    --logging_steps 10 \
-    --evaluation_strategy epoch \
-    --save_strategy epoch \
-    --load_best_model_at_end True \
-    --save_total_limit 3 \
-    --seed 1337
+python3 run_image_classification.py  \
+   --dataset_name cifar10  \
+   --output_dir ./cifar10_hbfp2,64/  \
+   --overwrite_output_dir \
+   --remove_unused_columns False  \
+   --do_train  \
+   --do_eval  \
+   --learning_rate 5e-5  \
+   --num_train_epochs 10  \
+   --per_device_train_batch_size 8  \
+   --per_device_eval_batch_size 8  \
+   --logging_strategy steps  \
+   --logging_steps 10  \
+   --evaluation_strategy epoch  \
+   --save_strategy epoch  \
+   --load_best_model_at_end True  \
+   --save_total_limit 5  \
+   --seed 42  \
+   --gradient_accumulation_steps 4  \
+   --adam_beta1 0.9  \
+   --adam_beta2 0.999  \
+   --adam_epsilon 1e-08  \
+   --lr_scheduler_type linear \
+   --optim sgd
