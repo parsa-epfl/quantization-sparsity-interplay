4045ad71b1ce3aa9820bda27eca793d027d09292
Ayan Chakraborty <ayan.chakraborty@epfl.ch>
2023-01-24 01:42:17 +0100

    small changes to scripts to run image_classification

---
+++ examples/pytorch/image-classification/run.sh
@@ -0,0 +1,17 @@
+python3 run_image_classification.py \
+    --dataset_name cifar100 \
+    --output_dir ./cifar100_test/ \
+    --remove_unused_columns False \
+    --do_train \
+    --do_eval \
+    --learning_rate 2e-5 \
+    --num_train_epochs 5 \
+    --per_device_train_batch_size 8 \
+    --per_device_eval_batch_size 8 \
+    --logging_strategy steps \
+    --logging_steps 10 \
+    --evaluation_strategy epoch \
+    --save_strategy epoch \
+    --load_best_model_at_end True \
+    --save_total_limit 3 \
+    --seed 1337
