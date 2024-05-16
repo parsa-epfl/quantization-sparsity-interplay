3944e46578304a2710a2d1a6b8ea9a71f964fb78
KostenokLisa <lisakostenok@gmail.com>
2024-05-16 15:28:45 +0000

    Per channel int8 quantization support

--- src/transformers/bfp/bfp_ops.py
+++ src/transformers/bfp/bfp_ops.py
@@ -29,6 +29,7 @@
 
 import torch
 import torch.nn.functional as F
+import torch.nn as nn
 from torch.optim import SGD
 import numpy as np
 import os
@@ -457,15 +458,128 @@ def rearrange_mats(matA, matB, device):
     matB = matB.reshape(orig_shape_B)
     return matA, matB
 
-def int_quantize(x, bits):
-    maxq = 2**bits-1
-    max_val = torch.max(x)
-    min_val = torch.min(x)
-    scale = (max_val-min_val)/maxq
-    zero = (-min_val)/scale
-    
+def quantize(x, scale, zero, maxq):
     q = torch.clamp(torch.round(x / scale) + zero, 0, maxq)
-    return scale*(q-zero)
+    return scale * (q - zero)
+
+class Quantizer(nn.Module):
+
+    def __init__(self, shape=1):
+        super(Quantizer, self).__init__()
+        self.register_buffer('maxq', torch.tensor(0))
+        self.register_buffer('scale', torch.zeros(shape))
+        self.register_buffer('zero', torch.zeros(shape))
+
+    def configure(
+            self,
+            bits, perchannel=True, sym=True, 
+            mse=False, norm=2.4, grid=100, maxshrink=.8,
+            grouprows=1
+        ):
+        self.maxq = torch.tensor(2 ** bits - 1)
+        self.perchannel = perchannel
+        self.sym = sym
+        self.mse = mse
+        self.norm = norm
+        self.grid = grid
+        self.maxshrink = maxshrink 
+        self.grouprows = grouprows
+
+    def find_params(self, x, weight=False):
+        dev = x.device
+        self.maxq = self.maxq.to(dev)
+
+        shape = x.shape
+        if self.perchannel:
+            if weight:
+                x = x.flatten(1)
+                if self.grouprows > 1: 
+                    x = x.reshape((x.shape[0] // self.grouprows, -1))
+            else:
+                if len(shape) == 4:
+                    x = x.permute([1, 0, 2, 3])
+                    x = x.flatten(1)
+                if len(shape) == 3:
+                    x = x.reshape((-1, shape[-1])).t()
+                if len(shape) == 2:
+                    x = x.t()
+        else:
+            x = x.flatten().unsqueeze(0)
+
+        tmp = torch.zeros(x.shape[0], device=dev)
+        xmin = torch.minimum(x.min(1)[0], tmp)
+        xmax = torch.maximum(x.max(1)[0], tmp)
+
+        if self.sym:
+            xmax = torch.maximum(torch.abs(xmin), xmax)
+            tmp = xmin < 0
+            if torch.any(tmp):
+                xmin[tmp] = -xmax[tmp]
+        tmp = (xmin == 0) & (xmax == 0)
+        xmin[tmp] = -1
+        xmax[tmp] = +1
+
+        self.scale = (xmax - xmin) / self.maxq
+        if self.sym:
+            self.zero = torch.full_like(self.scale, (self.maxq + 1) / 2)
+        else:
+            self.zero = torch.round(-xmin / self.scale)
+
+        if self.mse:
+            best = torch.full([x.shape[0]], float('inf'), device=dev)
+            for i in range(int(self.maxshrink * self.grid)):
+                p = 1 - i / self.grid 
+                xmin1 = p * xmin
+                xmax1 = p * xmax
+                scale1 = (xmax1 - xmin1) / self.maxq
+                zero1 = torch.round(-xmin1 / scale1) if not self.sym else self.zero
+                q = quantize(x, scale1.unsqueeze(1), zero1.unsqueeze(1), self.maxq)
+                q -= x
+                q.abs_()
+                q.pow_(self.norm)
+                err = torch.sum(q, 1)
+                tmp = err < best
+                if torch.any(tmp):
+                    best[tmp] = err[tmp]
+                    self.scale[tmp] = scale1[tmp]
+                    self.zero[tmp] = zero1[tmp]
+        if not self.perchannel:
+            if weight:
+                tmp = shape[0]
+            else:
+                tmp = shape[1] if len(shape) != 3 else shape[2]
+            self.scale = self.scale.repeat(tmp)
+            self.zero = self.zero.repeat(tmp)
+
+        if weight:
+            if self.grouprows > 1:
+                self.scale = self.scale.unsqueeze(1).repeat(1, self.grouprows)
+                self.zero = self.zero.unsqueeze(1).repeat(1, self.grouprows)
+            shape = [-1] + [1] * (len(shape) - 1)
+            self.scale = self.scale.reshape(shape)
+            self.zero = self.zero.reshape(shape)
+            return
+        if len(shape) == 4:
+            self.scale = self.scale.reshape((1, -1, 1, 1))
+            self.zero = self.zero.reshape((1, -1, 1, 1))
+        if len(shape) == 3:
+            self.scale = self.scale.reshape((1, 1, -1))
+            self.zero = self.zero.reshape((1, 1, -1)) 
+        if len(shape) == 2:
+            self.scale = self.scale.unsqueeze(0)
+            self.zero = self.zero.unsqueeze(0)
+
+    def quantize(self, x):
+        if self.ready():
+            return quantize(x, self.scale, self.zero, self.maxq)
+        return x
+
+    def enabled(self):
+        return self.maxq > 0
+
+    def ready(self):
+        return torch.all(self.scale != 0)
+
 
 def MxM_pre_processing(x, w, transpose, **bfp_args):
     device = bfp_args['device']
@@ -483,7 +597,14 @@ def MxM_pre_processing(x, w, transpose, **bfp_args):
             return (float_to_bfp_blocked(new_x, **bfp_args, identifier='in'), float_to_bfp_blocked(torch.transpose(new_w, -1, -2), **bfp_args, identifier='w'))
         else:
             # return (float_to_bfp_blocked(x, **bfp_args, identifier='in'), float_to_bfp_blocked(w, **bfp_args, identifier='w'))
-            return (int_quantize(x, 8), int_quantize(w, 8))
+            # return (int_quantize(x, 8), int_quantize(w, 8))
+            quantizer = Quantizer()
+            quantizer.configure(bits=8)
+            quantizer.find_params(x, weight=False)
+            q_x = quantizer.quantize(x)
+            quantizer.find_params(w, weight=True)
+            q_w = quantizer.quantize(w)
+            return (q_x, q_w)
 
 def float_to_bfp_batched(t, mant_bits, epsilon, rounding_mode, device, bfp_tile_size=25,
                          num_format='', weight_mant_bits=''):
